70414945d50960e37e4afc18bbe1018f5abada9e
estanislao01 <estani03@gmail.com>
2022-06-24 14:34:36 -0300

    correccion

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-{
+/* EJEMPLO 1 */
+
+
     const suma = (a,b) => a + b;
     const resta = (a,b) => a - b;
-    const iva = x => x * 0.21;
+    const iva = x => x * 0.21; 
+   
 
     let televisor= 8000;
     let descuento = 3000;
 
     let resultado = resta(suma(televisor,iva(televisor)),descuento)
     console.log(resultado);
-}
 
-{
-    const suma = (a,b) => {return a + b};
-    const resta = (a,b) => {return a - b};
+/* EJEMPLO 2 */
+
+
+    
     const multiplicacion = (a,b) => {return a * b};
     const impuesto = x => {return x*0.55}
 
     let alumnos= 25;
     let cuotas= 16000;
 
-    let resultado= multiplicacion(resta(cuotas, impuesto(cuotas)), alumnos)
+    let resultado1= multiplicacion(resta(cuotas, impuesto(cuotas)), alumnos)
 
-    console.log(resultado);
-}
+    console.log(resultado1);
